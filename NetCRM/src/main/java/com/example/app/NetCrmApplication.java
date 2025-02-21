312695dcad00d289d4e42651842007549421c6ba
none <none@com>
2025-02-21 13:02:42 +0900

    ver.0.0.1

---
+++ NetCRM/src/main/java/com/example/app/NetCrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetCrmApplication.class, args);
+	}
+
+}
